65f6a824bd5d9e0acd32f56cb750174ab19f0ab8
RoaaAbdelhamed <roaa.abdelhamed@student.giu-uni.de>
2025-11-12 15:43:57 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,17 @@
 #ifndef MAMMAL_H
 #define MAMMAL_H
-
 #include "Animal.h"
 
-//define mammal class here
+class Mammal : public Animal
+{
+private:
+    string furColor;
 
-
-
-
-
-
+public:
+    Mammal();
+    Mammal(string, int, bool, string);
+    void setFurColor(string);
+    string getFurColor();
+    void display() override;
+};
 #endif
