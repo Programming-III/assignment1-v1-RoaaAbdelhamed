e070cae9af7c77b59e17e792bb036efe98b73d92
RoaaAbdelhamed <roaa.abdelhamed@student.giu-uni.de>
2025-11-12 15:45:03 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,28 @@
-#ifndef REPTILE_H
-#define REPTILE_H
+#include "Reptile.h"
 
-#include "Animal.h"
+Reptile::Reptile() : Animal()
+{
+    isVenomous = false;
+}
 
-//define reptile class here
+Reptile::Reptile(string n, int a, bool h, bool v) : Animal(n, a, h)
+{
+    isVenomous = v;
+}
 
+void Reptile::setIsVenomous(bool v) { isVenomous = v; }
+bool Reptile::getIsVenomous() { return isVenomous; }
 
-
-
-
-
-
-
-
-#endif
+void Reptile::display()
+{
+    cout << getName() << "Age: " << getAge() << ", ";
+    if (isVenomous)
+        cout << "Venomous ";
+    else
+        cout << "Non-Venomous ";
+    if (getIsHungry())
+        cout << "Hungry"<emdl;
+    else
+        cout << "Not Hungry";
+    
+}
